3bb62f3a39cb5dbfc9ff6f0f46ae3c37a0e33a2d
Osama2138 <sharifkiviiri21@gmail.com>
2025-12-18 21:22:09 +0200

    worked on library transaction doctype

--- library_management_app/library_management_app/doctype/library_transaction/library_transaction.py
+++ library_management_app/library_management_app/doctype/library_transaction/library_transaction.py
@@ -1,9 +1,48 @@
 # Copyright (c) 2025, Osama and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model.docstatus import DocStatus
 
 
 class LibraryTransaction(Document):
-	pass
+
+    def before_submit(self):
+        if self.type == "Issue":
+            self.validate_issue()
+            article = frappe.get_doc("Article", self.article)
+            article.status = "Issued"
+            article.save()
+
+        elif self.type == "Return":
+            self.validate_return()
+            article = frappe.get_doc("Article", self.article)
+            article.status = "Available"
+            article.save()
+
+    def validate_issue(self):
+        self.validate_membership()
+        article = frappe.get_doc("Article", self.article)
+        if article.status == "Issued":
+            frappe.throw(f"{self.article} is not available")
+
+    def validate_return(self):
+        article = frappe.get_doc("Article", self.article)
+        if article.status == "Available":
+            frappe.throw("An article can't be returned if it wasn't issued")
+
+    def validate_membership(self):
+        valid_membership = frappe.db.exists(
+            "Library Membership",
+            {
+                "library_member": self.library_member,
+                "docstatus": DocStatus.submitted(),
+                "from_date": ("<=", self.date),
+                "to_date": (">=", self.date),
+            }
+        )
+
+        if not valid_membership:
+            frappe.throw(
+                f"{self.library_member} doesn't have a valid membership")
